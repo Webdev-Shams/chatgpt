d2640832afe03c23f8ca2154ac1693fb6f76badd
Webdev-Shams <webdev.shams@gmail.com>
2023-02-03 01:23:17 +0600

    background and text edited

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,7 @@ function App() {
   };
 
   return (
-    <div className="bg-white h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between  align-middle">
-      {/* gradients */}
-      <div className="gradient-01 z-0 absolute"></div>
-      <div className="gradient-02 z-0 absolute"></div>
-
-
-
+    <div className="whole bg-white h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between  align-middle">
 
       {/* body */}
       <div
@@ -40,8 +34,8 @@ function App() {
         <div className="uppercase font-extrabold text-black text-xl text-center mb-14 mx-auto">
           {/* header */}
           <img className="w-[150px] mx-auto" src="https://2.bp.blogspot.com/-sk1DC6JZT-0/XFWtXLIWxeI/AAAAAAAAGGo/GNdcu4n4p4kKvXTdxepFRDvRwyVLCWbnQCLcBGAs/s1600/JASHORE%2BUNIVERSITY%2BOF%2BSCIENCE%2B%2526%2BTECHNOLOGY%2B%2528JUST%2529%2B%2Bjust%2BNew%2B%2BLogo.png" alt="jessore university of science and technology" />
-          <p className="mt-6 ">Department of Marketing</p>
-          <span className="capitalize text-sm font-normal ">This website is for helping the students with their study by answering their questions.</span>
+          <p className="mt-6 text-white">Department of Marketing</p>
+          <span className="capitalize text-sm font-normal text-white">This website is for helping the students with their study by answering their questions.</span>
         </div>
         {/* header ends */}
         <ChatBody chat={chat} />
@@ -53,7 +47,7 @@ function App() {
       </div>
 
       <div className="mx-auto mt-5">
-        <p className="text-black">© developed by Shams Saif. </p>
+        <p className="font-thin">© developed by Shams Saif. </p>
       </div>
     </div>
   );
